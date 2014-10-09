61a7284cbe1341254a18d35847e3f49aa981b7f4
Michal Zajacik <michal.zajacik@agents.fel.cvut.cz>
2014-10-09 13:58:54 +0200

    examples and reading

--- 01_causeway/main.cpp
+++ 01_causeway/main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include "main.hpp"
 
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+
+#if DBG
+    cout << "Hello, world!" << endl;
+#else
+#endif
+    
+    
+#if DBG
+    ifstream fileStream ("../examples/pub01.out");
+    string buff;
+    getline(fileStream,buff);
+    cout << "Correct result: " << buff << endl;
+    fileStream.close();
+#endif
+
     return 0;
 }
